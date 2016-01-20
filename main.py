a98a133dbcb99b7747895ab0475a5c6213d1cb05
Ian Reynolds <idr@mit.edu>
2016-01-20 14:15:14 -0500

    First pass at Audacity NR algorithm

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from scipy.signal import *
 
 from record import Recorder
 from config import *
+from process import *
 
 class Pulse(object):
 	def __init__(self,start,end,duration):
@@ -15,60 +16,25 @@ class Pulse(object):
 		self.end = end
 		self.duration = duration
 
-def plot_spectrum(data):
-	time_step = 1.0 / RATE
-	ps = np.abs(np.fft.fft(data)) ** 2
-	freqs = np.fft.fftfreq(data.size, time_step)
-	idx = np.argsort(freqs)
-	plt.plot(freqs[idx], ps[idx])
+	def low(self):
+		return min(self.start,self.end)
 
-def chunks(l, n):
-	for i in xrange(0, len(l), n):
-		yield l[i:i+n]
-
-def frames_to_array(frames):
-	NORMALIZE = 1.0
-	res = []
-	for frame in frames:
-		format = "%df" % CHUNK
-		res = res + list(struct.unpack(format,frame))
-	arr = np.array(res) 
-	print arr
-	return arr * NORMALIZE
-
-def array_to_frames(arr):
-	NORMALIZE = 1.0
-	frames = []
-	arr = (arr * NORMALIZE)
-	print arr
-	res = np.ndarray.tolist(arr)
-	for chunk in chunks(res,CHUNK):
-		format = "%df" % CHUNK
-		frames.append(struct.pack(format,*chunk))
-	return frames
-
-def design_filter(lowcut, highcut, fs, order=3):
-	nyq = 0.5 * fs
-	low = lowcut / nyq
-	high = highcut / nyq
-	b,a = butter(order, [low,high], btype='band')
-	return b,a
-
-def bandpass(data,low,high):
-	bpf = design_filter(low,high,RATE)
-	return lfilter(bpf[0],bpf[1],data)
+	def high(self):
+		return max(self.start,self.end)
 
 def play_pulse(pulse,record_time=None):
 	if not record_time:
-		record_time = 100
-	t0 = time.time()
+		record_time = 200
 	frames = []
 	serial.write([pulse.start,pulse.end,pulse.duration])
 	for i, frame in enumerate(rec.record(float(record_time) / 1000)):
 		frames.append(frame)
 	data = frames_to_array(frames)
-	filtered = bandpass(data,30000,35000) * 10
-	new_frames = array_to_frames(filtered)
+	t0 = time.time()
+	data = bandpass(data,900 * pulse.low(),1100 * pulse.high()) * 3
+	# data = noise_reduce(data,trim(data,0.5,1)) * 3
+	print time.time() - t0
+	new_frames = array_to_frames(data)
 	stream = audio.open(
 		format=FORMAT,
 		channels=CHANNELS,
@@ -76,7 +42,7 @@ def play_pulse(pulse,record_time=None):
 		frames_per_buffer=CHUNK,
 		output=True
 	)
-	for f in new_frames:
+	for f in trim(new_frames,0,0.5):
 		stream.write(f)
 	stream.stop_stream()
 	stream.close()
@@ -85,9 +51,9 @@ def play_pulse(pulse,record_time=None):
 serial = serial.Serial(port=SERIAL_PORT,baudrate=SERIAL_BAUD,write_timeout=0.0)
 audio = pyaudio.PyAudio()
 rec = Recorder(audio)
-
-chirp_up = Pulse(30,50,5)
-bloop = Pulse(30,30,5)
+chirp = Pulse(30,70,10)
+bloop = Pulse(30,30,1)
 
 while True:
-	play_pulse(chirp_up)
+	play_pulse(chirp)
+
