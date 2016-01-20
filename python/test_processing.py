be6bd602a4ec217b8887635afbfa6b4185abc2fb
Ian Reynolds <idr@mit.edu>
2016-01-20 17:28:08 -0500

    Relative path correction

--- python/test_processing.py
+++ python/test_processing.py
@@ -3,7 +3,7 @@ from scikits.audiolab import wavread
 from noisereduce import noise_reduce, NoiseReduceSettings
 
 audio = pyaudio.PyAudio()
-sample, sample_freq, encoding = wavread('sounds/single-bloop.wav')
+sample, sample_freq, encoding = wavread('../sounds/single-bloop.wav')
 sample = bandpass(sample,30000,50000)
 noise = trim(sample,0,0.3)
 sample = noise_reduce(sample,noise,NoiseReduceSettings())
