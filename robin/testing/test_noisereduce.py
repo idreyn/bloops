2f6999facd7bf970efb3676d0985b08fd1588e80
Ian Reynolds <idreyn@gmail.com>
2017-02-24 19:19:22 -0500

    restructuring

--- robin/testing/test_noisereduce.py
+++ robin/testing/test_noisereduce.py
@@ -1,17 +1,15 @@
 import time, sys
-
-sys.path.append('..')
-sys.path.append('../noisereduce/')
-
-import pyximport
-pyximport.install()
-
 import pstats, cProfile
 
+import sounddevice as sd
 from scikits.audiolab import wavread
+
+sys.path.append('..')
+
+
+from robin.plotting import *
 from robin.process import *
-from robin.noisereduce.noisereduce import noise_reduce
-from robin.noisereduce.settings import NoiseReduceSettings
+from robin.noisereduce import noise_reduce, NoiseReduceSettings
 
 # audio = pyaudio.PyAudio()
 
@@ -22,15 +20,10 @@ def noise_reduce_test():
 	t0 = time.time()
 	sample = noise_reduce(sample,noise,NoiseReduceSettings(window_size=64))
 	print 'noise filter in time:', round(time.time() - t0,2)
-	'''
 	sample = from_db(20) * sample
 	if True:
-		data = pad_to_size(sample,CHUNK * (1 + len(sample) / CHUNK))
-		data = array_to_frames(data)
-		play_frames(audio,data,0.04)
-	'''
-
-
+		sd.play(sample, rate)
+		
 cProfile.runctx("res = noise_reduce_test()",globals(),locals(),"profile.prof")
 s = pstats.Stats("profile.prof")
 s.strip_dirs().sort_stats("time").print_title()
