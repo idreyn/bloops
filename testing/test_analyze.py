dfa8e9be047af039c7852076978a1cd658b5f7d0
Ian Reynolds <idreyn@gmail.com>
2017-03-14 18:14:15 -0400

    working on alignment

--- testing/test_analyze.py
+++ testing/test_analyze.py
@@ -1,4 +1,9 @@
-import test
+import test, time
 from scikits.audiolab import *
 from process.analyze import *
 
+sample = wavread("../../samples/weak-chirp.wav")[0]
+t0 = time.time()
+a = align(sample)
+t1 = time.time()
+print a, t1 - t0
