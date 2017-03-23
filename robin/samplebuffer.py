e0a3c02545b8b870f5d231c17bb9102610d14159
Ian Reynolds <idreyn@gmail.com>
2017-03-23 14:18:11 -0400

    this was not a triumph

--- robin/samplebuffer.py
+++ robin/samplebuffer.py
@@ -1,11 +1,13 @@
 import time
 import bisect
 import numpy as np
+from multiprocessing import Manager
 
 
 class SampleBuffer(object):
 
     def __init__(self, channels, period_capacity=500):
+        self.manager = Manager()
         self.queue = []
         self.times = []
         self.channels = channels
@@ -21,8 +23,10 @@ class SampleBuffer(object):
             self.shift()
 
     def shift(self, ln=1):
+        res = self.queue[0:ln]
         self.queue = self.queue[ln:]
         self.times = self.times[ln:]
+        return res
 
     def pointer_for_time(self, time, start_limit=0, end_limit=None):
         if not end_limit:
@@ -35,13 +39,15 @@ class SampleBuffer(object):
         self.shift(offset)
         pointer = 0
         buff = np.zeros((length, self.channels))
+        i = 0
         while pointer < length:
+            i = i + 1
             if not len(self.queue):
-                print "underflow", pointer
                 break
             else:
                 sample = self.queue[0]
                 take = min(length - pointer, len(sample))
+                t0 = time.time()
                 buff[pointer: pointer + take, :] = sample[0:take, :]
                 self.queue[0] = sample[take:, :]
                 pointer = pointer + take
