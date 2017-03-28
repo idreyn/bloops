9daf4e11c137751496ed70f712978fb22deec22f
Ian Reynolds <idreyn@gmail.com>
2017-03-27 22:11:05 -0400

    threads work at least a little bit

--- samplebuffer.py
+++ samplebuffer.py
@@ -1,42 +1,44 @@
 import time
+import threading
 import bisect
 import numpy as np
-from multiprocessing import Manager
 
+from util import zero_pad
 
 class SampleBuffer(object):
 
     def __init__(self, channels, period_capacity=500):
-        self.manager = Manager()
         self.queue = []
         self.times = []
         self.channels = channels
+        self.lock = threading.Lock()
         self.period_capacity = period_capacity
 
-    def time_range(self):
-        return (self.times[0], self.times[-1])
-
-    def put(self, sample):
-        self.queue.append(np.copy(sample))
-        self.times.append(time.time())
-        if len(self.queue) > self.period_capacity:
-            self.shift()
-
-    def shift(self, ln=1):
+    def _shift(self, ln=1):
         res = self.queue[0:ln]
         self.queue = self.queue[ln:]
         self.times = self.times[ln:]
         return res
+
+    def time_range(self):
+        return (self.times[0], self.times[-1])
 
     def pointer_for_time(self, time, start_limit=0, end_limit=None):
         if not end_limit:
             end_limit = len(self.times)
         midpoint = (start_limit + end_limit) / 2
 
+    def put_samples(self, sample):
+        self.queue.append(np.copy(sample))
+        self.times.append(time.time())
+        if len(self.queue) > self.period_capacity:
+            self._shift()
+
     def get_samples(self, length, start_time=None):
+        self.lock.acquire()
         offset = 0 if start_time is None \
             else bisect.bisect_left(self.times, start_time)
-        self.shift(offset)
+        self._shift(offset)
         pointer = 0
         buff = np.zeros((length, self.channels))
         i = 0
@@ -52,5 +54,6 @@ class SampleBuffer(object):
                 self.queue[0] = sample[take:, :]
                 pointer = pointer + take
                 if not len(self.queue[0]):
-                    self.shift()
+                    self._shift()
+        self.lock.release()
         return buff
